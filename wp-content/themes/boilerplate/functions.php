e68ac955f273244798521152447ad43e862fa90b
Nyssa Jayne <nyssajayne@gmail.com>
2017-04-07 18:22:10 -0400

    tidying up the header

--- wp-content/themes/boilerplate/functions.php
+++ wp-content/themes/boilerplate/functions.php
@@ -18,13 +18,60 @@ function register_styles() {
 add_action( 'wp_enqueue_scripts', 'register_scripts' );
 
 function register_scripts() {
-	//wp_register_script( 'scripts', get_stylesheet_directory_uri() . '/js/scripts.js' );
+	wp_register_script( 'scripts', get_stylesheet_directory_uri() . '/js/scripts.js' );
 
-	//wp_enqueue_script( 'scripts' );
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'scripts' );
 }
 
 add_action( 'after_setup_theme', 'register_menu' );
 
 function register_menu() {
   register_nav_menu( 'primary', __( 'Primary Menu', 'theme-slug' ) );
+}
+
+function print_menu($menu_type) {
+	$defaults = array(
+		'theme_location'  => 'primary',
+		'menu'            => '',
+		'container'       => 'nav',
+		'container_class' => 'nav-desktop ',
+		'container_id'    => '',
+		'menu_class'      => 'vertical-text',
+		'menu_id'         => '',
+		'echo'            => true,
+		'fallback_cb'     => 'wp_page_menu',
+		'before'          => '',
+		'after'           => '',
+		'link_before'     => '',
+		'link_after'      => '',
+		'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+		'depth'           => 0,
+		'walker'          => ''
+	);
+
+	$defaults_mob = array(
+		'theme_location'  => 'primary',
+		'menu'            => '',
+		'container'       => 'nav',
+		'container_class' => 'nav-mob',
+		'container_id'    => '',
+		'menu_class'      => 'vertical-text',
+		'menu_id'         => '',
+		'echo'            => true,
+		'fallback_cb'     => 'wp_page_menu',
+		'before'          => '',
+		'after'           => '',
+		'link_before'     => '',
+		'link_after'      => '',
+		'items_wrap'      => '<ul id="%1$s" class="%2$s"><li id="nav-menu-mob">Menu</li>%3$s</ul>',
+		'depth'           => 0,
+		'walker'          => ''
+	);
+
+	if($menu_type == 'mobile') {
+		wp_nav_menu( $defaults_mob );
+	} else {
+		wp_nav_menu( $defaults );
+	}
 }
